d8e6423f8e515d58bd44d27ec931b5e92cf0c852
kratt15 <sidneyadjoh15@gmail.com>
2024-11-02 18:37:52 +0000

    Add front_url variable d'env and bouncer

--- start/env.ts
+++ start/env.ts
@@ -35,5 +35,10 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
   SMTP_HOST: Env.schema.string(),
-  SMTP_PORT: Env.schema.string()
+  SMTP_PORT: Env.schema.string(),
+
+  /* Frontend URL */
+  FRONT_URL: Env.schema.string(),
+
+
 })
