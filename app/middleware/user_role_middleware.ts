2c9de33c89c003ebd70c49e59abc17595cd38d63
kratt15 <sidneyadjoh15@gmail.com>
2024-11-03 01:07:52 +0000

    role middleware  and others updates

---
+++ app/middleware/user_role_middleware.ts
@@ -0,0 +1,15 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import type { NextFn } from '@adonisjs/core/types/http'
+
+export default class UserRoleMiddleware {
+  async handle(ctx: HttpContext, next: NextFn) {
+
+    const user = ctx.auth.user
+
+    if (!user  || user.isAdmin == false) {
+      return ctx.response.status(403).send({ message: 'Unauthorized access' })
+    }
+    const output = await next()
+    return output
+  }
+}
